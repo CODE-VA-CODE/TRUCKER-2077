495d93432486728ef81f41055bca52413904ab63
NemoSemi <yaSemiYT@bk.ru>
2022-12-25 21:14:48 +0500

    Почти доделано MainMenu и полностью реализован класс для создания кнопок

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("TRUCKER-2077")
-print('Pycharm говно')
+import sys
+import pygame
+from MainMenu import MainMenuButtonGroup
+from configfile import clock, FPS
+
+
+while True:
+    for event in pygame.event.get():
+        MainMenuButtonGroup.update(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
+    clock.tick(FPS)
